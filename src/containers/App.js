9fce2cd099ffe50e7f7d19ebc175f53588fd12f9
mahesh-nikam-explicate <mahesh.nikam@explicate.in>
2022-08-25 15:47:58 +0530

    Secon Commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,27 @@
 import './App.css';
 import CardList from '../component/CardList/CardList';
-import { robots } from '../constants/robots';
+// import { robots } from '../constants/robots';
 import SearchBox from '../component/SearchBox/SearchBox';
-import React ,{ Component } from 'react';
+import React ,{ useState, useEffect } from 'react';
 // import LifeCycle from './component/LifeCycle/LifeCycle.js';
 import Scroll from '../component/Scroll/Scroll';
 import ErrorBoundry from '../component/ErrorBoundry/ErrorBoundry';
 
-class App extends Component {
+function App() {
 
-  constructor() {
-    super();
+  const [robots, setRobots] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-    this.state = { 
-      'robots': [],
-      'searchText': ''
-     };
+  const onSearchChange = (event) => {
+   setSearchText(event.target.value);
   }
 
-  onSearchChange = (event) => {
-    this.setState({robots: robots, searchText: event.target.value});
-  }
-
-  componentDidMount() {
+  useEffect(()=> {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(responce =>responce.json())
-    .then(users => this.setState({robots: users}));
-  }
-
-  render() {
-    const {robots, searchText} = this.state;
+    .then(users => setRobots(users));
+  }, []);
+   
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchText);
     })
@@ -38,7 +30,7 @@ class App extends Component {
         (<div className="tc"><h1>Loading...</h1></div>) 
         : (<div className="tc">
         <h1 className="f2">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
         <CardList robotsArr={filteredRobots}/>
@@ -46,7 +38,6 @@ class App extends Component {
         </Scroll>
         {/* <LifeCycle/> */}
       </div>)
-  }
 }
 
 export default App;
